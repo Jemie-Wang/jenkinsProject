a38d593952347f31fdcdd467e383d7cf57667de1
“Jingyi <jw979@cornell.edu>
2023-11-12 21:31:39 -0500

    Initial project

---
+++ src/main/java/org/example/BasicCalculator.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class BasicCalculator {
+    public int add(int number1, int number2){
+        return number1 + number2;
+    }
+
+    public int minus(int number1, int number2){
+        return number1 - number2;
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,25 @@
+import org.example.BasicCalculator;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MainTest {
+
+    BasicCalculator calculator;
+
+    @Before
+    public void setup(){
+        calculator = new BasicCalculator();
+    }
+    @Test
+    public void testAdd(){
+        int val1 = 9, val2 = 10;
+        Assert.assertEquals(calculator.add(val1, val2), 19);
+    }
+
+    @Test
+    public void testMinus(){
+        int val1 = 10, val2 = 9;
+        Assert.assertEquals(calculator.minus(val1, val2), 1);
+    }
+}
